04bf8224ca7c20e3c800912671c0abd341ad1ddf
Colin Rowlands <ccrowlands@gmail.com>
2018-08-30 12:38:24 -0300

    Set up formatting for amount and date

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,4 +6,4 @@ import expenses from "../fixtures/expenses";
 test("should render expense list item with an expense", () => {
     const wrapper = shallow(<ExpenseListItem {...expenses[1]}/>);
     expect(wrapper).toMatchSnapshot();
-});
+}); 
